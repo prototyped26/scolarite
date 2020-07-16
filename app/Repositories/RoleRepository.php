5bb5b8517bb7b0dcb99eeeaab2a27967b7da2c15
Ornel Zamo <prototyped26@gmail.com>
2020-07-16 12:25:31 +0000

    Ajout des interfaces de l'auth et les données statics dans le Role Repos

--- app/Repositories/RoleRepository.php
+++ app/Repositories/RoleRepository.php
@@ -7,8 +7,26 @@ use App\Models\Role;
 
 class RoleRepository extends BaseRepository
 {
+    /**
+     * @var Role
+     */
+    public static $_ADMIN;
+
+    /**
+     * @var Role
+     */
+    public static $_INTENDANT;
+
+    /**
+     * @var Role
+     */
+    public static $_CAISSIER;
+
     public function __construct(Role $model)
     {
         parent::__construct($model);
+        self::$_ADMIN = $this->model->where('code', 'ADM')->get()->first();
+        self::$_INTENDANT = $this->model->where('code', 'INT')->get()->first();
+        self::$_CAISSIER = $this->model->where('code', 'CAS')->get()->first();
     }
 }
