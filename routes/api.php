08afb572f97a58f0cd2b492e162b3542926586ca
liongeant <mveabraham@gmail.com>
2020-07-16 23:48:42 +0100

    Ajout de la route pour obtenir l'utilisateur connecté et du Refresh Token sur le Kernel

--- routes/api.php
+++ routes/api.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user()->load(['role']);
+    return $request->user();
 });
 
 Route::group(['middleware' => ['auth:api']], function () {
@@ -35,3 +35,7 @@ Route::apiResources([
     'caracteristiques' => 'Api\CaracteristiqueController',
     'motifs' => 'Api\MotifController',
 ]);
+
+Route::group(['prefix' => 'users'], function (){
+   Route::get('current', 'Api\UserController@currentUser');
+});
